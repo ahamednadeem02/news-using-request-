5fd81490af2664b8f03f6cf33033213394023a2e
ahamednadeem02 <111524475+ahamednadeem02@users.noreply.github.com>
2023-04-10 23:01:58 +0530

    final

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 from send_email import send_email
 
 api_key = "890603a55bfa47048e4490069ebee18c"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "sortBy=publishedAt&apiKey=" \
-      "890603a55bfa47048e4490069ebee18c"
+url = "https://newsapi.org/v2/everything?" \
+      "q=tesla&" \
+      "sortBy=publishedAt&" \
+      "apiKey=890603a55bfa47048e4490069ebee18c&" \
+      "language=en"
 
 # Make request
 request = requests.get(url)
@@ -14,9 +16,12 @@ content = request.json()
 
 # Access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+        body = "Subject: Today's news" \
+        + "\n" + body + article["title"] + "\n" \
+        + article["description"] \
+        + "\n" + article["url"] +  2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
